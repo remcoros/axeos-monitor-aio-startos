60fb9ff6b5946c6da5471c82f67785c1bdb3ab0e
Remco Ros <remcoros@live.nl>
2026-04-15 10:39:15 +0200

    chore: update release notes with translations

--- startos/versions/v1.0.0.ts
+++ startos/versions/v1.0.0.ts
@@ -3,8 +3,11 @@ import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 export const v1_0_0 = VersionInfo.of({
   version: '1.0.0:1-beta.1',
   releaseNotes: {
-    en_US:
-      'Update to StartOS SDK 1.0.0, Grafana 12.4.3, Prometheus 3.11.2, fix provisioning race condition on startup',
+    en_US: 'Initial release of AxeOS Monitor for StartOS 0.4',
+    es_ES: 'Lanzamiento inicial de AxeOS Monitor para StartOS 0.4',
+    de_DE: 'Erstveröffentlichung von AxeOS Monitor für StartOS 0.4',
+    pl_PL: 'Pierwsze wydanie AxeOS Monitor dla StartOS 0.4',
+    fr_FR: 'Première version de AxeOS Monitor pour StartOS 0.4',
   },
   migrations: {
     up: async ({ effects }) => {},
