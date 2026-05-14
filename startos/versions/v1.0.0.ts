441689504074f7ba7e85c240c4e5e2a9952abca9
Remco Ros <remcoros@live.nl>
2026-05-14 18:52:36 +0200

    chore: release 1.0.0:1

--- startos/versions/v1.0.0.ts
+++ startos/versions/v1.0.0.ts
@@ -1,7 +1,7 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const v1_0_0 = VersionInfo.of({
-  version: '1.0.0:1-beta.3',
+  version: '1.0.0:1',
   releaseNotes: {
     en_US: 'Initial release of AxeOS Monitor for StartOS 0.4',
     es_ES: 'Lanzamiento inicial de AxeOS Monitor para StartOS 0.4',
